b95126e42ae3b668f33eb777a800bf9d1c3bf5e7
chenqim <chenqim@51bsi.com>
2017-07-28 17:38:57 +0800

    简易天气爬虫

---
+++ spider_weather_simple.py
@@ -0,0 +1,50 @@
+'''
+参考博客：http://www.cnblogs.com/coltfoal/archive/2012/10/06/2713348.html
+'''
+
+# coding : UTF-8
+
+import urllib.request
+import re
+import codecs
+from bs4 import BeautifulSoup
+
+#定义一个写入文本的函数
+def writetxt(path, content, code):
+    with codecs.open(path, 'a', encoding=code) as f:
+        f.write(content)
+    return path + ' is ok!'
+
+def get_html(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    html = html.decode('utf-8')
+    page.close()
+    return html
+
+def get_weather(html):
+    final = []
+    bs = BeautifulSoup(html, "html.parser")  # 创建BeautifulSoup对象
+    body = bs.body # 获取body部分
+    ul = body.find('ul', {'class': 'clearfix city'})  # 找到class为clearfix city的ul
+    # ul = data.find('ul')  # 获取ul部分
+    # li = ul.find_all('li')  # 获取所有的li
+    # weatherList = re.compile(reg).findall(html)
+    li = ul.find_all('li')
+    for day in li:
+        temp = []
+        city = day.find('span').string
+        tempurature = day.find('i').string
+        temp.append(city)
+        temp.append(tempurature)
+        final.append(temp)
+    return final
+
+if __name__ == '__main__':
+    # url = 'http://nb.zj.weather.com.cn/'
+    url = 'http://www.weather.com.cn/weather1d/101210401.shtml#input'
+    html = get_html(url)
+    # print(html)
+    weather = str(get_weather(html))
+    print(writetxt('tianqi2.txt', weather, 'utf-8'))
+    print(weather)
